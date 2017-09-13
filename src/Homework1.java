df67ee1a147d009093016e23a90562b32d425939
Wojciech Sobasik <wojciech.sobasik@gmail.com>
2017-09-13 14:40:14 +0200

    drugi commit (dodanie "i commitowania")

---
+++ src/Homework1.java
@@ -0,0 +1,10 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
+
+    }
+}
