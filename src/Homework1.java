5db3f2b6346c27451d864529bb3b0fc6c72eb85f
Wojciech Sobasik <wojciech.sobasik@gmail.com>
2017-09-13 14:43:08 +0200

    trzeci commit (dodanie "odrabiam zadania domowe")

--- src/Homework1.java
+++ src/Homework1.java
@@ -5,6 +5,10 @@ public class Homework1 {
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        System.out.println("");
+        System.out.println("Odrabiam");
+        System.out.println("zadania");
+        System.out.println("domowe");
 
     }
 }
